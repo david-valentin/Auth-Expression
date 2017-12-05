6484aaf82b7d056edf2684c72b2bb4affd2199a8
David Valentin <davidvalentin@Davids-MacBook-Air.local>
2017-12-05 20:57:01 +0000

    updated load-users.js and populated database

--- backend/seeds/load-users.js
+++ backend/seeds/load-users.js
@@ -13,7 +13,13 @@ exports.seed = function(knex, Promise) {
       }),
     knex('types').del()
       .then(function() {
-        return knex('types')
+        return knex('types').insert([
+          {id: 1, name: 'Girl Scout Cookies', type:"Hybrid", description:
+          "Girl Scout Cookies, or GSC, is an OG Kush and Durban Poison hybrid cross whose reputation grew too large to stay within the borders of its California homeland. With a sweet and earthy aroma, Girl Scout Cookies launches you to euphoria’s top floor where full-body relaxation meets a time-bending cerebral space. A little goes a long way with this hybrid, whose THC heights have won Girl Scout Cookies numerous Cannabis Cup awards. Patients needing a strong dose of relief, however, may look to GSC for severe pain, nausea, and appetite loss."},
+          {id: 2, name: 'Blue Dream', type:"Hybrid", description:
+          "Blue Dream, a sativa-dominant hybrid originating in California, has achieved legendary status among West Coast strains. Crossing a Blueberry indica with the sativa Haze, Blue Dream balances full-body relaxation with gentle cerebral invigoration. Novice and veteran consumers alike enjoy the level effects of Blue Dream, which ease you gently into a calm euphoria. Some Blue Dream phenotypes express a more indica-like look and feel, but the sativa-leaning variety remains most prevalent."
+          }
+        ])
 
       })
   ]
