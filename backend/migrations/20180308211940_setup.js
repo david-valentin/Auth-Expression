5a3673d4b637a172ad6889712cbb6f16265d9327
David Valentin <david.y.valentin@gmail.com>
2018-03-08 21:48:26 -0500

    Fixed backend signup with password encryption

--- backend/migrations/20180308211940_setup.js
+++ backend/migrations/20180308211940_setup.js
@@ -9,12 +9,12 @@ const businessesTable = function(table){
 
 const usersTable = function(table){
   table.integer('id').primary();
-  table.string('email');
+  table.string('email').notNullable();
   table.unique('email');
-  table.string('password');
-  table.string('hall');
-  table.string('room_num');
-  table.string('parking_lot');
+  table.string('password').notNullable();
+  table.string('hall').notNullable();
+  table.string('roomNum').notNullable();
+  table.string('parking_lot').notNullable();
   table.dateTime('member_since');
 }
 
@@ -28,14 +28,20 @@ const ordersTable = function(table){
 
 
 exports.up = function(knex, Promise) {
-  return Promise.all([knex.schema.createTable('users', usersTable), knex.schema.createTable('orders', ordersTable), knex.schema.createTable('businessesTable', businessesTable)])
+  return Promise.all([
+    knex.schema.createTable('users', usersTable),
+    knex.schema.createTable('orders', ordersTable),
+    knex.schema.createTable('businessesTable', businessesTable)])
   .then(() => {
     console.log('Database created');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([knex.schema.dropTable('users', usersTable), knex.schema.dropTable('orders', ordersTable), knex.schema.dropTable('businessesTable', businessesTable)])
+  return Promise.all([
+    knex.schema.dropTable('users', usersTable),
+    knex.schema.dropTable('orders', ordersTable),
+    knex.schema.dropTable('businessesTable', businessesTable)])
   .then(() => {
     console.log('Database Destroyed');
   });
